42ab06ff018f6f756c390d8ce110fbe671465bdc
Joseph Chen <joe1chen@hotmail.com>
2013-08-09 13:03:35 -0700

    Add rainbows attributes

--- rainbows/attributes/default.rb
+++ rainbows/attributes/default.rb
@@ -4,9 +4,13 @@ default[:rainbows][:worker_processes] = node[:rails][:max_pool_size] ? node[:rai
 default[:rainbows][:backlog] = 1024
 default[:rainbows][:timeout] = 60
 default[:rainbows][:preload_app] = true
-default[:rainbows][:version] = '4.0.1'
+default[:rainbows][:version] = '4.5.0'
 default[:rainbows][:tcp_nodelay] = true
 default[:rainbows][:tcp_nopush] = false
 default[:rainbows][:tries] = 5
 default[:rainbows][:delay] = 0.5
 default[:rainbows][:accept_filter] = "httpready"
+
+# Rainbows Specific Attributes
+default[:rainbows][:concurrency] = 'EventMachine'
+default[:rainbows][:worker_processes] = 100
