6d9f9b88e074685c06e76c88f1b456859efe0177
Joseph Chen <joe1chen@hotmail.com>
2013-08-22 08:23:57 -0700

    Cleanup

--- opsworks_nodejs_custom/recipes/symlink.rb
+++ opsworks_nodejs_custom/recipes/symlink.rb
@@ -1,5 +1,3 @@
-#if ::File.exist?('/usr/bin/node') && !::File.exist?('/usr/local/bin/node')
-  link '/usr/local/bin/node' do
-    to '/usr/bin/node'
-  end
-#end
+link '/usr/local/bin/node' do
+  to '/usr/bin/node'
+end
